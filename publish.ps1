3ab9dcf44fdc434ed0d33d8a77711be2b1482348
Björn Lindqvist <bjourne@gmail.com>
2013-03-19 17:32:38 +0100

    pub

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,8 @@
 .\dev.ps1
 git add -f static
+git add -f config.py
 git commit . -m "temp heroku deploy commit"
 git push heroku master --force
 git reset --soft HEAD~1
 git reset HEAD static
+git reset HEAD config.py
