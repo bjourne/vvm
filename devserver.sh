68679e0ef5f0a4ef9f70135a707163f1a600276d
bjourne@gmail.com <bjourne@gmail.com>
2013-03-23 13:02:04 +0100

    fix

--- devserver.sh
+++ devserver.sh
@@ -2,16 +2,17 @@
 set -eu
 
 
-cp -v src/js/* static/js/
-cp -v src/images/* static/images/*
 mkdir -p static/css
 mkdir -p static/images
+cp -v src/js/* static/js/
+cp -rv src/images/* static/images/
+
 
 coffee -wbj out.js -o static/js src/coffee &
 jade -wP src/*.jade -O static &
 
 jade -wP src/partials -O static/partials &
-stylus -w -o static/css src/styl/style.styl
+stylus -w -o static/css src/styl/style.styl &
 # Python doesn't like to be terminated for unknown reason.
 /usr/bin/python app.py &
 
